9be0446709d6f9c58b984f8ca85622305354339d
xililo <sililosililo82@gmail.com>
2023-02-04 22:52:18 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+// Variables: - declaration - initialization - assignment
+var num;
+num = 5;
+var num2 = 7;
+var msg = "Hello. what is your name?";
+//var user = prompt('some other message');
+//console.log(user);
+
+var username; //undefined
+let y = 5;
+const userid = 1234;
+var statement = "helo there";
+var second_statement = 'we are okay';
+var thirdStatement = "i'm fine"; //camelCase 
+var finalStatement = 'the statement is ' + statement + ' ' + userid;
+var finalStatement2 = `the statement is  ${statement}, the user id is ${userid}`;
+console.log(finalStatement2);
+var sum = y +""+ userid; 
+//when adding a number to a string you have converted the number into a string
---
+++ javascript.js
@@ -0,0 +1,66 @@
+/*
+using a while loop find the sum of the even numbers between  1 and 100;
+*/
+
+
+
+/*
+while (num < 100) { // when num = 100, returns false;
+    sum = sum + num;
+    num = num + 2; // increment by 2;
+}
+ */
+/* var num = 0;
+var sum = 0;
+while (num < 100) {
+
+    let remainder = num % 2;
+    if (remainder == 0) {
+        sum = sum + num;
+    }
+    num++;
+}
+
+*/
+
+//FOR LOOPS
+/* var sum = 0;
+
+for (let i = 0; i < 100; i++) {
+    let remainder = i % 2;
+    if (remainder == 0) {
+        sum = sum + i;
+    }
+}
+let answer = `<h1> The answer is <u>${sum}</u> </h1>`;
+document.write(answer); */
+
+// FUNCTIONS
+
+
+/* 
+
+var value = add(3, 4);
+var value2 = add(33, 44);
+
+console.log('valu1 is ' + value);
+console.log('valu2 is ' + value2); */
+
+/*create a funtion that adds two numbers, divides them, 
+and returns the product of their quotient and sum;
+*/
+var add = function (a, b) {
+    var ans = a + b;
+    return ans;
+}
+var calc = function (x, y) {
+    let q = x / y;
+    let s = x + y;
+    let prod = q * s;
+    return prod;
+}
+
+var value1 = calc(46, 72);
+var value2 = calc(8, 5);
+var final = add(value1, value2);
+console.log(final)
